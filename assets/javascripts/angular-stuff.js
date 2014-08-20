52da46f685287995245edacded14ee6a5063182e
Eoin O'Brien <eoin@eoinobrien.ie>
2014-08-20 23:32:00 +0100

    Updated Files

---
+++ assets/javascripts/angular-stuff.js
@@ -0,0 +1,923 @@
+var data = [
+{
+  title : "Accommodation Documents",
+  category : "Important Documents",
+  essential : true
+},
+{
+  title : "Bank Details",
+  category : "Important Documents",
+  essential : true
+},
+{
+  title : "Driving License",
+  category : "Important Documents",
+  essential : false
+},
+{
+  title : "Garda Age Card",
+  category : "Important Documents",
+  essential : true
+},
+{
+  title : "Grant Details",
+  category : "Important Documents",
+  essential : true
+},
+{
+  title : "Health Insurance Details",
+  category : "Important Documents",
+  essential : false
+},
+{
+  title : "Medical Card",
+  category : "Important Documents",
+  essential : false
+},
+{
+  title : "Passport",
+  category : "Important Documents",
+  essential : false
+},
+{
+  title : "Passport Photos",
+  category : "Important Documents",
+  essential : false
+},
+{
+  title : "A4 folders",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "A4 paper",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "Books",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "Calculator",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "Desk lamp",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "Dictionary",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "Glue",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "Highlighters",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "Hole Punch",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "Jotter/Reporter pad",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "Labels",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "Lined paper",
+  category : "Study Material",
+  essential : true
+},
+{
+  title : "Paper clips",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "Pencils",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "Pens",
+  category : "Study Material",
+  essential : true
+},
+{
+  title : "Pins for notice board",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "Plastic wallets",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "Post-it notes",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "Ruler",
+  category : "Study Material",
+  essential : true
+},
+{
+  title : "School bag",
+  category : "Study Material",
+  essential : true
+},
+{
+  title : "Scissors",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "Sellotape",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "Stapler",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "White tack",
+  category : "Study Material",
+  essential : false
+},
+{
+  title : "Colour Catchers",
+  category : "Laundry",
+  essential : false
+},
+{
+  title : "Laundry Bag",
+  category : "Laundry",
+  essential : false
+},
+{
+  title : "Spare Change",
+  category : "Laundry",
+  essential : false
+},
+{
+  title : "Washing Power/Liquid Tablets",
+  category : "Laundry",
+  essential : true
+},
+{
+  title : "Computer/Laptop & Charger",
+  category : "Computer Equipment",
+  essential : true
+},
+{
+  title : "Laptop Bag",
+  category : "Computer Equipment",
+  essential : false
+},
+{
+  title : "Printer",
+  category : "Computer Equipment",
+  essential : false
+},
+{
+  title : "USB Pen",
+  category : "Computer Equipment",
+  essential : true
+},
+{
+  title : "Cushions",
+  category : "Decorative",
+  essential : false
+},
+{
+  title : "Cushions/Beanbags",
+  category : "Decorative",
+  essential : false
+},
+{
+  title : "Extra Storage",
+  category : "Decorative",
+  essential : false
+},
+{
+  title : "Inflatable Mattress and Pump",
+  category : "Decorative",
+  essential : false
+},
+{
+  title : "Photos",
+  category : "Decorative",
+  essential : false
+},
+{
+  title : "Posters",
+  category : "Decorative",
+  essential : false
+},
+{
+  title : "Rug",
+  category : "Decorative",
+  essential : false
+},
+{
+  title : "Sleeping Bag",
+  category : "Decorative",
+  essential : false
+},
+{
+  title : "Board Games",
+  category : "Leisure",
+  essential : false
+},
+{
+  title : "DVD Player",
+  category : "Leisure",
+  essential : false
+},
+{
+  title : "Football",
+  category : "Leisure",
+  essential : false
+},
+{
+  title : "Hurley & Sliotar",
+  category : "Leisure",
+  essential : false
+},
+{
+  title : "MP3 Player/IPod",
+  category : "Leisure",
+  essential : false
+},
+{
+  title : "Pack of Cards",
+  category : "Leisure",
+  essential : true
+},
+{
+  title : "Playstation/Xbox/Wii",
+  category : "Leisure",
+  essential : false
+},
+{
+  title : "Rugby Ball",
+  category : "Leisure",
+  essential : false
+},
+{
+  title : "Sports Equipment/Sportsbag/Gym Gear",
+  category : "Leisure",
+  essential : false
+},
+{
+  title : "Television",
+  category : "Leisure",
+  essential : false
+},
+{
+  title : "TV Ariel with Booster",
+  category : "Leisure",
+  essential : false
+},
+{
+  title : "Blanket",
+  category : "Bedding",
+  essential : false
+},
+{
+  title : "Duvet",
+  category : "Bedding",
+  essential : true
+},
+{
+  title : "Duvet cover",
+  category : "Bedding",
+  essential : true
+},
+{
+  title : "Hot water bottle",
+  category : "Bedding",
+  essential : false
+},
+{
+  title : "Mattress protector",
+  category : "Bedding",
+  essential : true
+},
+{
+  title : "Pillow Cases",
+  category : "Bedding",
+  essential : true
+},
+{
+  title : "Pillows",
+  category : "Bedding",
+  essential : true
+},
+{
+  title : "Posters",
+  category : "Bedding",
+  essential : false
+},
+{
+  title : "Sheets",
+  category : "Bedding",
+  essential : true
+},
+{
+  title : "Bowls",
+  category : "Kitchen",
+  essential : true
+},
+{
+  title : "Forks",
+  category : "Kitchen",
+  essential : true
+},
+{
+  title : "Knives",
+  category : "Kitchen",
+  essential : true
+},
+{
+  title : "Mugs",
+  category : "Kitchen",
+  essential : true
+},
+{
+  title : "Pint glasses",
+  category : "Kitchen",
+  essential : false
+},
+{
+  title : "Plastic cups",
+  category : "Kitchen",
+  essential : false
+},
+{
+  title : "Plates",
+  category : "Kitchen",
+  essential : true
+},
+{
+  title : "Shot glasses",
+  category : "Kitchen",
+  essential : false
+},
+{
+  title : "Spoons",
+  category : "Kitchen",
+  essential : true
+},
+{
+  title : "Wine glasses",
+  category : "Kitchen",
+  essential : false
+},
+{
+  title : "Chopping board",
+  category : "Kitchen",
+  essential : true
+},
+{
+  title : "Chopping knives",
+  category : "Kitchen",
+  essential : true
+},
+{
+  title : "Corkscrew / Bottle opener",
+  category : "Kitchen",
+  essential : false
+},
+{
+  title : "Grater",
+  category : "Kitchen",
+  essential : false
+},
+{
+  title : "Scissors",
+  category : "Kitchen",
+  essential : true
+},
+{
+  title : "Sieve",
+  category : "Kitchen",
+  essential : false
+},
+{
+  title : "Spatula",
+  category : "Kitchen",
+  essential : false
+},
+{
+  title : "Tin opener",
+  category : "Kitchen",
+  essential : true
+},
+{
+  title : "Vegetable peeler",
+  category : "Kitchen",
+  essential : true
+},
+{
+  title : "Wooden spoon",
+  category : "Kitchen",
+  essential : false
+},
+{
+  title : "Cling Film",
+  category : "Kitchen",
+  essential : true
+},
+{
+  title : "Febreze",
+  category : "Kitchen",
+  essential : false
+},
+{
+  title : "Oven glove",
+  category : "Kitchen",
+  essential : false
+},
+{
+  title : "Plastic boxes with lids",
+  category : "Kitchen",
+  essential : true
+},
+{
+  title : "Sandwich bags",
+  category : "Kitchen",
+  essential : false
+},
+{
+  title : "Tea towel",
+  category : "Kitchen",
+  essential : false
+},
+{
+  title : "Tin foil",
+  category : "Kitchen",
+  essential : true
+},
+{
+  title : "Toaster bags",
+  category : "Kitchen",
+  essential : false
+},
+{
+  title : "Washing up brush",
+  category : "Kitchen",
+  essential : false
+},
+{
+  title : "Washing up liquid",
+  category : "Kitchen",
+  essential : false
+},
+{
+  title : "Baking tray",
+  category : "Kitchen",
+  essential : false
+},
+{
+  title : "Frying pan",
+  category : "Kitchen",
+  essential : true
+},
+{
+  title : "Saucepan small & large, with lids",
+  category : "Kitchen",
+  essential : true
+},
+{
+  title : "Baked Beans",
+  category : "Basic Foods",
+  essential : true
+},
+{
+  title : "Biscuits",
+  category : "Basic Foods",
+  essential : true
+},
+{
+  title : "Pasta",
+  category : "Basic Foods",
+  essential : true
+},
+{
+  title : "Pasta Sauce",
+  category : "Basic Foods",
+  essential : true
+},
+{
+  title : "Rice",
+  category : "Basic Foods",
+  essential : true
+},
+{
+  title : "Sweets",
+  category : "Basic Foods",
+  essential : true
+},
+{
+  title : "Tinned food",
+  category : "Basic Foods",
+  essential : true
+},
+{
+  title : "Tomato ketchup",
+  category : "Basic Foods",
+  essential : true
+},
+{
+  title : "Basket for Shower Supplies",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Conditioner",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Mouthwash",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Shampoo",
+  category : "Bathroom",
+  essential : true
+},
+{
+  title : "Shower Gel",
+  category : "Bathroom",
+  essential : true
+},
+{
+  title : "Soap",
+  category : "Bathroom",
+  essential : true
+},
+{
+  title : "Toothbrush",
+  category : "Bathroom",
+  essential : true
+},
+{
+  title : "Toothpaste",
+  category : "Bathroom",
+  essential : true
+},
+{
+  title : "Aftershave / Perfume",
+  category : "Bathroom",
+  essential : true
+},
+{
+  title : "Comb",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Deodorant",
+  category : "Bathroom",
+  essential : true
+},
+{
+  title : "Hair Gel / Hair Spray / Hair Products",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Hairbrush",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Nail Clippers",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Razor",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Shaving Cream/Gel",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Wash Bag",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Anti-Allergy Pills",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Basic First Aid Kit",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Cold/Flu Remedy",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Cold/Hot Pack",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Condoms",
+  category : "Bathroom",
+  essential : true
+},
+{
+  title : "Cough Bottle",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Lip Balm",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Multivitamins",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Painkillers",
+  category : "Bathroom",
+  essential : true
+},
+{
+  title : "Pill",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Bath Mat",
+  category : "Bathroom",
+  essential : false
+},
+{
+  title : "Bath Towel",
+  category : "Bathroom",
+  essential : true
+},
+{
+  title : "Hand Towel",
+  category : "Bathroom",
+  essential : true
+},
+{
+  title : "County Jersey",
+  category : "Clothing",
+  essential : false
+},
+{
+  title : "Dresses",
+  category : "Clothing",
+  essential : false
+},
+{
+  title : "Dressing Gown",
+  category : "Clothing",
+  essential : false
+},
+{
+  title : "Fancy Dress Supplies",
+  category : "Clothing",
+  essential : false
+},
+{
+  title : "Flip Flops",
+  category : "Clothing",
+  essential : false
+},
+{
+  title : "Going out Clothes",
+  category : "Clothing",
+  essential : true
+},
+{
+  title : "Going out shoes / High Heels / Pumps",
+  category : "Clothing",
+  essential : false
+},
+{
+  title : "Jeans",
+  category : "Clothing",
+  essential : true
+},
+{
+  title : "Jumpers",
+  category : "Clothing",
+  essential : true
+},
+{
+  title : "Pyjamas",
+  category : "Clothing",
+  essential : false
+},
+{
+  title : "Runners",
+  category : "Clothing",
+  essential : true
+},
+{
+  title : "Shirt and Tie",
+  category : "Clothing",
+  essential : false
+},
+{
+  title : "Shoes",
+  category : "Clothing",
+  essential : true
+},
+{
+  title : "Slippers",
+  category : "Clothing",
+  essential : false
+},
+{
+  title : "Socks",
+  category : "Clothing",
+  essential : true
+},
+{
+  title : "Swim Wear",
+  category : "Clothing",
+  essential : false
+},
+{
+  title : "Trainers",
+  category : "Clothing",
+  essential : true
+},
+{
+  title : "Trousers",
+  category : "Clothing",
+  essential : true
+},
+{
+  title : "T-Shirts",
+  category : "Clothing",
+  essential : true
+},
+{
+  title : "Umbrella",
+  category : "Clothing",
+  essential : false
+},
+{
+  title : "Underwear",
+  category : "Clothing",
+  essential : true
+},
+{
+  title : "Warm Coat",
+  category : "Clothing",
+  essential : true
+},
+{
+  title : "Candles",
+  category : "Other",
+  essential : false
+},
+{
+  title : "Coat Hangers",
+  category : "Other",
+  essential : false
+},
+{
+  title : "Duster",
+  category : "Other",
+  essential : false
+},
+{
+  title : "Alcohol",
+  category : "Other",
+  essential : false
+},
+{
+  title : "Loose change",
+  category : "Other",
+  essential : false
+},
+{
+  title : "Wallet",
+  category : "Other",
+  essential : false
+},
+{
+  title : "Phone and Charger",
+  category : "Other",
+  essential : false
+},
+{
+  title : "Alarm Clock",
+  category : "Other",
+  essential : true
+},
+{
+  title : "Batteries",
+  category : "Other",
+  essential : false
+},
+{
+  title : "Camera",
+  category : "Other",
+  essential : false
+},
+{
+  title : "Extension Leads",
+  category : "Other",
+  essential : false
+},
+{
+  title : "Long Ethernet Cable",
+  category : "Other",
+  essential : false
+},
+{
+  title : "Loud Speaker",
+  category : "Other",
+  essential : false
+},
+{
+  title : "Headphones",
+  category : "Other",
+  essential : true
+},
+{
+  title : "Door Wedge",
+  category : "Other",
+  essential : false
+},
+{
+  title : "Hair Dryer",
+  category : "Other",
+  essential : false
+},
+{
+  title : "Hair Straightener/Curling Tongs",
+  category : "Other",
+  essential : false
+}
+];
+var categories = ["All","Important Documents","Study Material","Laundry","Computer Equipment","Decorative","Leisure","Bedding","Kitchen","Basic Foods","Clothing","Other"];
+
+var ttttcollege = angular.module('ttttcollege', []);
+
+ttttcollege.controller('ItemsController', function($scope) {
+  $scope.items = data;
+  $scope.categories = categories;
+  $scope.currentCat = categories[0];
+
+  $scope.remaining = function() {
+    var count = 0;
+    angular.forEach($scope.items, function(item) {
+      count += item.done ? 0 : 1;
+    });
+    return count;
+  };
+  $scope.filterByCategory = function(category) {
+    var list = data;
+    $scope.items = [];
+    angular.forEach(list, function(item) {
+      if(item.category === category) $scope.items.push(item);
+    });
+    $scope.currentCat = category;
+  };
+
+});
